78c1c5f8850208a4c84631db85e282f1740be859
Relebohile Khamali <Relebohile.Khamali@capaciti.org.za>
2024-01-31 11:24:30 +0200

    (initial commit)

--- HackerQ3/src/HackerQ3.java
+++ HackerQ3/src/HackerQ3.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
-public class HackerrQ3 {
+public class HackerQ3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
 
         int i = scan.nextInt();
-
         double d = scan.nextDouble();
         scan.nextLine();
         String s = scan.nextLine();
@@ -15,7 +14,7 @@ public class HackerrQ3 {
         System.out.println("Double: " + d);
         System.out.println("Int: " + i );
 
-        
+
 
     }
 
